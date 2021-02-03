730ca159fd8c54fcfcc3cf800e2d9d58c0f8fc98
Heverson <desenvolverdorh@gmail.com>
2021-02-03 11:02:35 -0300

    atualização das 11:02

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -1,12 +1,22 @@
 sap.ui.define([
-   "sap/ui/core/mvc/Controller",
-    "sap/m/MessgeToast"
-], function(Controller){
-    "use strict"
-    return Controller.extend("sap.ui.demo.walkthrough.App",{
-        onShowHello : function(){
-            alert("fala meu brother!");
-            MessageToast.show("fala Meu Broother!")
-        }
+    "sap/ui/core/mvc/Controller",
+    "sap/m/MessageToast",
+    "sap/ui/model/json/JSONModel"
+ ], function (Controller, MessageToast, JSONModel) {
+    "use strict";
+    return Controller.extend("sap.ui.demo.walkthrough.controller.App", {
+       onInit : function () {
+          // set data model on view
+          var oData = {
+             recipient : {
+                name : "Bem-vindo!"
+             }
+          };
+          var oModel = new JSONModel(oData);
+          this.getView().setModel(oModel);
+       },
+       onShowHello : function () {
+          MessageToast.show("Vamos fazer acontecer?");
+       }
     });
-});
+ });
